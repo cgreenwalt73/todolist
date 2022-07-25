718cb91d802c30d7433da4fd3c93bfe35f21c132
Cody Greenwalt <cgreenwalt73@gmail.com>
2022-07-24 23:53:20 -0400

    to do list assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const [list, setList] = useState([]);
+  const [newListItem, setNewListItem] = useState('');
+
+  const addListItem = (e) => {
+    e.preventDefault();
+    
+    if (newListItem === '') {
+      return;
+    }
+
+    const listItem = {
+      text: newListItem,
+      complete: false
+    }
+    setList([...list, listItem]);
+    setNewListItem('');
+};
+
+  const deleteListItem = (indexToDelete) => {
+    const filteredList = list.filter((listItem, index) => {
+      return (index !== indexToDelete)
+    });
+    setList(filteredList);
+  }
+
+  const completeTask = (listItem) => {
+    listItem.complete = !listItem.complete;
+    setList([...list]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To Do List:</h1>
+      <form onSubmit= { (e) => {addListItem(e);} }>
+        <label>
+          <input type='text' value={newListItem} onChange={ (e) => setNewListItem(e.target.value) }></input>
+          <input type='submit' ></input>
+        </label>
+      </form>
+
+      {
+        list.map((listItem, index) => {
+          return(
+            <div key={index}>
+              <input onChange={ (e) => {
+                completeTask(listItem);
+              }}
+              checked={list.complete} type='checkbox'/>
+              {listItem.text}
+              <button onClick={(e) => {deleteListItem(index);} }>Delete</button>
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
